e7e922998dad74ca18ef363b0fcc827eab67ab31
Christoph Landgraf <christoph.landgraf@gmail.com>
2018-01-24 14:39:01 +0100

    Missing import added

--- cui_source/util.py
+++ cui_source/util.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 
 def mode_line_file_str(path, mode_line_columns=None):
     """
